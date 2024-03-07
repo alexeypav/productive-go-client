ad1406cbfeecabf38740f934d297589cc2ccdd18
alexey pavlenko <alexey.pavlenko@clearpoint.co.nz>
2024-03-08 11:16:10 +1300

    Added option to enter time via command with flags

--- cmd/productive-go-client/main.go
+++ cmd/productive-go-client/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"productive-go-client/internal/config"
 	"productive-go-client/internal/data"
@@ -12,64 +16,85 @@ import (
 	"github.com/cqroot/prompt"
 )
 
+var (
+	serviceId = flag.String("serviceId", "", "ID of the service")
+	date      = flag.String("date", time.Now().Format("2006-01-02"), "Date for the operation in YYYY-MM-DD format")
+	hours     = flag.Int("hours", 8, "Hours component of the time")
+	minutes   = flag.Int("minutes", 0, "Minutes component of the time")
+	notes     = flag.String("notes", "", "Date for the operation in YYYY-MM-DD format")
+	baseURL   = "https://api.productive.io/api/v2/"
+)
+
 func main() {
 	fmt.Println("Checking Config")
 
 	cfg, err := config.LoadAndVerifyConfig()
 	if err != nil {
-		fmt.Println("Error:", err)
-		return
+		log.Fatal("Error:", err)
 	}
 
 	client := &http.Client{}
-	baseURL := "https://api.productive.io/api/v2/"
 
-	var productiveService data.ProductiveService
-	productiveService = data.NewProductive(client, baseURL)
+	productiveService := data.NewProductive(client, baseURL)
 	timeService := service.NewTimeService(productiveService)
 
 	user, err := timeService.Repo.GetUser(cfg)
 	if err != nil {
-		fmt.Println("Error:", err)
-		return
+		log.Fatal("Error:", err)
 	}
 
 	ui.DisplayUserDetails(&user)
 	if err != nil {
-		fmt.Printf("Error in %s \n", err)
+		log.Fatalf("Error in %s \n", err)
 	}
 
 	if err := config.UpdateConfigWithUserID(&cfg, user.ID); err != nil {
-		fmt.Println("Failed to update user ID:", err)
-		return
+		log.Print("Failed to update user ID:", err)
 	}
 
-	for {
-		choice, err := prompt.New().Ask("Choose:").Choose([]string{"Enter Time", "Show Time Codes", "Exit"})
+	//If flags are passed, handle and run, otherwise show the ui
+	if len(os.Args) > 1 {
+		flag.Parse()
+
+		if *serviceId == "" {
+			log.Fatal("No Service ID provided via flags...")
+		}
+
+		err = timeService.EnterTime(&user, &cfg, *date, *serviceId, *notes, *hours, *minutes)
 		if err != nil {
-			fmt.Println("Error:", err)
-			return
+			log.Fatal("Failed to create a time entry", err)
 		}
+		log.Print("Successfully entered time for: ", *serviceId, *date)
 
-		switch choice {
-		case "Enter Time":
-			err := ui.EnterTime(&user, timeService, &cfg)
+	} else {
+
+		for {
+			choice, err := prompt.New().Ask("Choose:").Choose([]string{"Enter Time", "Show Time Codes", "Exit"})
 			if err != nil {
-				fmt.Printf("Error in %s \n", err)
+				fmt.Println("Error:", err)
 			}
 
-		case "Show Time Codes":
-			ui.ShowUserTimeCodes(timeService, &cfg)
-			if err != nil {
-				fmt.Printf("Error in %s \n", err)
+			switch choice {
+			case "Enter Time":
+				err := ui.EnterTime(&user, timeService, &cfg)
+				if err != nil {
+					fmt.Printf("Error in %s \n", err)
+				}
+
+			case "Show Time Codes":
+				ui.ShowUserTimeCodes(timeService, &cfg)
+				if err != nil {
+					fmt.Printf("Error in %s \n", err)
+				}
+
+			case "Exit":
+				fmt.Println("Exiting...")
+				return
+
+			default:
+				fmt.Println("Invalid choice. Please select a valid option.")
 			}
-
-		case "Exit":
-			fmt.Println("Exiting...")
-			return
-
-		default:
-			fmt.Println("Invalid choice. Please select a valid option.")
 		}
+
 	}
 }
